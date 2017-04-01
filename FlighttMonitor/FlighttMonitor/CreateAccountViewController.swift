b8cbdc53d286e9201d191fad108dd980e92ef0ea
Alexandra Lazea <medeealazea@yahoo.com>
2017-04-02 00:49:27 +0300

    authenticate user

---
+++ FlighttMonitor/FlighttMonitor/CreateAccountViewController.swift
@@ -0,0 +1,42 @@
+//
+//  CreateAccountViewController.swift
+//  FlighttMonitor
+//
+//  Created by Alexandra Lazea on 02/04/2017.
+//  Copyright © 2017 Medeea. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var checkPasswordField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func createAccount(_ sender: UIButton) {
+        
+    }
+}
--- FlighttMonitor/FlighttMonitor/ViewController.swift
+++ FlighttMonitor/FlighttMonitor/ViewController.swift
@@ -7,19 +7,94 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var logoutButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let user = FIRAuth.auth()?.currentUser {
+            self.logoutButton.alpha = 1.0
+            self.usernameLabel.text = user.email
+        }
+        else {
+            self.logoutButton.alpha = 0.0
+            self.usernameLabel.text = ""
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func createAccount(_ sender: UIButton) {
+        
+        performSegue(withIdentifier: "loginToCreateAccount", sender: self)
+        
+        /*if self.emailField.text == "" || self.passwordField.text == "" {
+            self.showAlert(title: "Error", message: "Alege user si parola")
+        }
+        else {
+            FIRAuth.auth()?.createUser(withEmail: self.emailField.text!, password: self.passwordField.text!, completion: { (user, error) in
+                if error == nil {
+                    self.logoutButton.alpha = 1.0
+                    self.usernameLabel.text = user!.email
+                    self.clearFields()
+                }
+                else{
+                    self.showAlert(title: "Error", message: (error?.localizedDescription)!)
+                }
+            })
+        }
+*/
+    }
+    
+    @IBAction func loginAction(_ sender: UIButton) {
+        if self.emailField.text == "" || self.passwordField.text == "" {
+            self.showAlert(title: "Error", message: "Alege user si parola")
+        }
+        else {
+            FIRAuth.auth()?.signIn(withEmail: self.emailField.text!, password: self.passwordField.text!, completion: { (user, error) in
+                if error == nil {
+                    self.logoutButton.alpha = 1.0
+                    self.usernameLabel.text = user!.email
+                    self.clearFields()
+                }
+                else{
+                    self.showAlert(title: "Error", message: (error?.localizedDescription)!)
+                }
+            })
+        }
+    }
+    
+    @IBAction func logoutAction(_ sender: UIButton) {
+        try! FIRAuth.auth()?.signOut()
+        self.clearFields()
+        self.logoutButton.alpha = 0.0
+        self.usernameLabel.text = ""
+    }
 
+    func showAlert(title: String, message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: { (action) in
+            alert.dismiss(animated: true, completion: nil)
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func clearFields(){
+        self.passwordField.text = ""
+        self.emailField.text = ""
+
+    }
 
 }
 
