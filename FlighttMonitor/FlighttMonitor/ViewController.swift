50fb52b45c3b3f869ea933587748a827bd8eb89c
Alexandra Lazea <medeealazea@yahoo.com>
2017-04-02 15:01:21 +0300

    added facebook login

--- FlighttMonitor/FlighttMonitor/ViewController.swift
+++ FlighttMonitor/FlighttMonitor/ViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 import Firebase
 import FirebaseAuth
 import FirebaseDatabase
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
 
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var emailField: UITextField!
@@ -36,7 +37,28 @@ class ViewController: UIViewController {
             self.logoutButton.alpha = 0.0
             self.usernameLabel.text = ""
         }
+        
+        
+        let fbLoginButton = FBSDKLoginButton()
+        fbLoginButton.frame = CGRect(x: 16, y: 50, width: view.frame.width - 32, height: 50)
+        fbLoginButton.delegate = self
+        view.addSubview(fbLoginButton)
 
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if error != nil{
+            showAlert(title: "Error", message: (error?.localizedDescription)!)
+        }
+        else{
+            print("a mers")
+            showAlert(title: "Success", message: "Login successful.", segueName: "loginToFirstView")
+            
+        }
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("bravo")
     }
 
     override func didReceiveMemoryWarning() {
@@ -90,6 +112,21 @@ class ViewController: UIViewController {
         }))
         self.present(alert, animated: true, completion: nil)
     }
+    
+    func showAlert(title: String, message: String, segueName: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: { (action) in
+            if (segueName == "") {
+                alert.dismiss(animated: true, completion: nil)
+            }
+            else{
+                self.performSegue(withIdentifier: segueName, sender: true)
+            }
+            
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+
     
     func clearFields(){
         self.passwordField.text = ""
